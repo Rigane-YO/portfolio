79c468228fb33a74308f1075404e24eb7232cfac
Rigane-YO <rigane.rakotozanany@gmail.com>
2025-01-30 14:47:43 +0100

    ajout de google analytic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./component/Home";
+import TrackPageView from "./util/TrackPageView";
 import './App.css'
 
 function App() {
   return (
     <Router>
+       <TrackPageView />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from './component/ThemeContext.tsx';
+import ReactGA from "react-ga4";
 import './index.css';
+// Initialisation de Google Analytics
+ReactGA.initialize("G-4XFRPFGQV2");
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
